e2e79081bc41ab8e05263315ffacf243d5b3d2e8
cch09906667 <carlos_chavez@outlook.com>
2013-08-18 11:43:52 -0500

    Terminado ejemplo 1

---
+++ EjemploMDI/IMantenimiento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploMDI
+{
+    public interface IMantenimiento
+    {
+        void CrearNuevo(object sender, EventArgs e);
+        void ModificarActual(object sender, EventArgs e);
+        void EliminarActual(object sender, EventArgs e);
+        void Ver(object sender, EventArgs e);
+        void RefrescarDatos(object sender, EventArgs e);
+        void Aceptar(object sender, EventArgs e);
+        void Cancelar(object sender, EventArgs e);
+        void Salir(object sender, EventArgs e);
+    }
+}
